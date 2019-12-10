68e33bcd1ddb3b744a6234803f43e10c2313511a
Pelle Månsson <pelle.mansson@attentec.se>
2019-12-10 09:11:02 +0100

    Advent 8

--- src/main.rs
+++ src/main.rs
@@ -795,8 +795,78 @@ mod advent_09 {
     }
 }
 
+mod advent_08 {
+    pub fn count_chars(s: &str, ccc: char) -> usize {
+        s.chars().filter(|c| c == &ccc).count()
+    }
+    pub fn uh(input: &str, w: usize, h: usize) {
+        let mut input_m = input;
+        let mut layers = vec![];
+        loop {
+            if input_m.len() == 0 {
+                break;
+            }
+            let (image_data, rest) = input_m.split_at(w * h);
+            input_m = rest;
+            layers.push(image_data);
+        }
+        let layer_with_fewest_0_digits = layers.iter().min_by_key(|s| count_chars(s, '0')).unwrap();
+        println!("{}", layer_with_fewest_0_digits);
+        println!(
+            "{}",
+            count_chars(layer_with_fewest_0_digits, '1')
+                * count_chars(layer_with_fewest_0_digits, '2')
+        );
+    }
+
+    pub fn decode_pixel(i: usize, layers: &Vec<&str>) -> char {
+        for layer in layers {
+            match layer.chars().nth(i).unwrap() {
+                '0' => return ' ',
+                '1' => return '#',
+                '2' => {}
+                _ => panic!(),
+            }
+        }
+        panic!();
+    }
+
+    pub fn uh2(input: &str, w: usize, h: usize) {
+        let mut input_m = input;
+        let mut layers = vec![];
+        loop {
+            if input_m.len() == 0 {
+                break;
+            }
+            let (image_data, rest) = input_m.split_at(w * h);
+            input_m = rest;
+            layers.push(image_data);
+        }
+        for y in 0..h {
+            for x in 0..w {
+                print!("{}", decode_pixel(y * w + x, &layers));
+            }
+            println!("");
+        }
+    }
+
+    pub fn test1() {
+        uh("123456789012", 3, 2);
+    }
+    pub fn main1(input: Vec<String>) {
+        uh(&input.join(""), 25, 6);
+    }
+    pub fn test2() {
+        uh2("0222112222120000", 2, 2);
+    }
+    pub fn main2(input: Vec<String>) {
+        uh2(&input.join(""), 25, 6);
+    }
+}
+
 fn main() {
-    //advent_09::test1();
-    advent_09::main1(read_input("inputs/input9"));
-    advent_09::main2(read_input("inputs/input9"));
+    advent_08::test1();
+    advent_08::main1(read_input("inputs/input8"));
+    advent_08::test2();
+    advent_08::main2(read_input("inputs/input8"));
 }
